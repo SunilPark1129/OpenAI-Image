6a18e5f8b7639415f93a0aef2cc8418d28e62fa5
Sunil Park <sunilpark1129@gmail.com>
2023-02-06 15:36:12 -0500

    Added Display result text

--- src/components/Display/Screen.jsx
+++ src/components/Display/Screen.jsx
@@ -4,7 +4,10 @@ const Screen = ({ error, resImage, prompt }) => {
   return error !== null ? (
     <div className="error">{error}</div>
   ) : (
-    <img src={resImage} alt={prompt} />
+    <>
+      <div className="prompt">{prompt}</div>
+      <img src={resImage} alt={prompt} />
+    </>
   );
 };
 
