e3bc44283d4f94eafdaea4d6559ffddfeddb38b8
Sunil Park <sunilpark1129@gmail.com>
2023-02-09 17:59:18 -0500

    Updated Accept Input

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -45,7 +45,7 @@ const InputBox = ({
           ref={refReset}
           onChange={onChangeHandler}
           disabled={isLoading && !unmounted}
-          accept="image/*;capture=camera"
+          accept="image/jpeg,image/png;capture=camera"
         />
       </label>
     </section>
